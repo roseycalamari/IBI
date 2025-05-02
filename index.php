db350284e53edb0e0f7b9e8dc1f30b25cea2fedd
roseycalamari <rosaruben7@gmail.com>
2025-05-02 23:50:27 +0100

    m

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
  * Main entry point
  */
 
+// Redirect mobile.html requests to index.html
+$requestUri = $_SERVER['REQUEST_URI'];
+if (strpos($requestUri, 'mobile.html') !== false) {
+    $redirectUrl = str_replace('mobile.html', 'index.html', $requestUri);
+    header("Location: $redirectUrl", true, 301);
+    exit;
+}
+
 // Include the main website
 include 'index.html';
 ?> 
